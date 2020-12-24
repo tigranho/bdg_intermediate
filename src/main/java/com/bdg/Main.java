f912d6eb7d1111751d1d9e534fac3ba982431c04
Martin <martin-88@mail.ru>
2020-12-24 19:42:53 +0400

    Test Martin

--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Test");
-        System.out.println("Test Martin");
+        System.out.println("Test MartinM");
 
 
 
