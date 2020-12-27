9d5d4d9e4bd292215a707782462a0ca158b6b2da
ruzan.zohrabyan <ruzanna83@gmail.com>
2020-12-27 22:07:48 +0400

    Add new branch

--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -1,12 +1,16 @@
 package com.bdg;
 
+import java.sql.SQLOutput;
+
 /**
  * @author Tigran
  */
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Test");
+        System.out.println("Hello Ruzanna");
     }
 
 }
