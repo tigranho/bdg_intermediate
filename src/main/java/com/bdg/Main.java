cf58637c26a1733c625e261c32a7aabb4db601e2
Argishti Tigranyan <tigranyan.argishti@gmail.com>
2020-12-27 13:41:16 +0400

    add aggregation example

--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -4,6 +4,9 @@ package com.bdg;
  * @author Argishti
  */
 
+/**
+ * Class description: Simple java beans example
+ */
 class Person
 {
     private String name, surname;
@@ -58,11 +61,120 @@ class Person
     }
 }
 
+/**
+ * Class Description: Simple Composition and Aggregation example
+ */
+class Employee
+{
+    private String id;
+    private String firstName;
+    private String lastName;
 
-public class Main {
+    //Aggreation example
+    private Address address;
 
-    public static void main(String[] args) {
-        System.out.println("Test");
+    public Employee()
+    {
+
     }
 
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getId()
+    {
+        return this.id;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+    public void setAddress(Address address)
+    {
+        this.address = address;
+    }
+
+    public Address getAddress()
+    {
+        return this.address;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Employee: [id: "+this.id+", first name: "+this.firstName+", last name: "+this.lastName+", address: "+this.address+"]";
+    }
+}
+
+class Address
+{
+    private String country;
+    private String city;
+    private String street;
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getCountry()
+    {
+        return this.country;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getCity()
+    {
+        return this.city;
+    }
+
+    public void setStreet(String street)
+    {
+        this.street = street;
+    }
+
+}
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Address address = new Address();
+        address.setCountry("Armenia");
+        address.setCity("Yerevan");
+        address.setStreet("Nairi Zaryan");
+
+        Employee employee = new Employee();
+        employee.setId("001");
+        employee.setFirstName("Karapet");
+        employee.setLastName("Grigoryan");
+        employee.setAddress(address);
+
+        System.out.println(employee);
+
+    }
 }
