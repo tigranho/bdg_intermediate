1838692dc102834df979ed8826c5c718d7552722
ruzan.zohrabyan <ruzanna83@gmail.com>
2020-12-29 13:32:35 +0400

    Add HayStorage class

---
+++ src/main/java/com/bdg/LlamaTrainer.java
@@ -0,0 +1,15 @@
+package com.bdg;
+
+public class LlamaTrainer {
+    public boolean feedLlamas(int numberOfLlamas) {
+        int feedNeeded = 5 * numberOfLlamas;
+        HayStorage hayStorage = HayStorage.getInstance();
+        if (hayStorage.getHayQuantity() < feedNeeded) {
+            hayStorage.addHay(feedNeeded + 10);
+        }
+        boolean fed = hayStorage.removeHay(feedNeeded);
+        if (fed)
+            System.out.println("Llamas have been fed");
+        return fed;
+    }
+}
