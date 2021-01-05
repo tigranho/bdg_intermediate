2102715d887258290a685d4b7b46ae4f8fd58590
ruzan.zohrabyan <ruzanna83@gmail.com>
2021-01-06 00:00:54 +0400

    CustomArrayList finished

--- src/main/java/CustomDataStructures/CustomArrayList.java
+++ src/main/java/CustomDataStructures/CustomArrayList.java
@@ -73,7 +73,7 @@ public class CustomArrayList<E> implements List<E> {
     public boolean contains(Object o) {
         if (o == null) {
             throw new NullPointerException();
-        } else if (!(o.getClass().isAssignableFrom(array[0].getClass()))) {
+        } else if (!isEmpty() && !(o.getClass().isAssignableFrom(array[0].getClass()))) {
             throw new ClassCastException();
         }
         return indexOf(o) >= 0;
@@ -152,7 +152,7 @@ public class CustomArrayList<E> implements List<E> {
     public boolean remove(Object o) {
         if (o == null) {
             throw new NullPointerException();
-        } else if (!(o.getClass().isAssignableFrom(array[0].getClass()))) {
+        } else if (!isEmpty() && !(o.getClass().isAssignableFrom(array[0].getClass()))) {
             throw new ClassCastException();
         }
         int index = indexOf(o);
@@ -178,9 +178,49 @@ public class CustomArrayList<E> implements List<E> {
         return false;
     }
 
+    /**
+     * Appends all of the elements in the specified collection to the end of
+     * this list, in the order that they are returned by the specified
+     * collection's iterator (optional operation).  The behavior of this
+     * operation is undefined if the specified collection is modified while
+     * the operation is in progress.  (Note that this will occur if the
+     * specified collection is this list, and it's nonempty.)
+     *
+     * @param c collection containing elements to be added to this list
+     * @return {@code true} if this list changed as a result of the call
+     * @throws UnsupportedOperationException if the {@code addAll} operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of an element of the specified
+     *                                       collection prevents it from being added to this list
+     * @throws NullPointerException          if the specified collection contains one
+     *                                       or more null elements and this list does not permit null
+     *                                       elements, or if the specified collection is null
+     * @throws IllegalArgumentException      if some property of an element of the
+     *                                       specified collection prevents it from being added to this list
+     * @see #add(Object)
+     */
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        if (c == null || c.contains(null)) {
+            throw new NullPointerException();
+        }
+        Object[] o = c.toArray();
+        if (o.length >= array.length - size) {
+            extendArrayIfNeeded();
+        }
+        E[] array1 = (E[]) new Object[size + o.length];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            array1[j] = array[i];
+            j++;
+        }
+        for (int i = 0; i < o.length; i++) {
+            array1[j] = (E) o[i];
+            j++;
+        }
+        array = array1;
+        size += o.length;
+        return true;
     }
 
     @Override
@@ -198,24 +238,101 @@ public class CustomArrayList<E> implements List<E> {
         return false;
     }
 
+    /**
+     * Removes all of the elements from this list (optional operation).
+     * The list will be empty after this call returns.
+     *
+     * @throws UnsupportedOperationException if the {@code clear} operation
+     *                                       is not supported by this list
+     */
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
     }
 
+    /**
+     * Returns the element at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   ({@code index < 0 || index >= size()})
+     */
     @Override
     public E get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
     }
 
+    /**
+     * Replaces the element at the specified position in this list with the
+     * specified element (optional operation).
+     *
+     * @param index   index of the element to replace
+     * @param element element to be stored at the specified position
+     * @return the element previously at the specified position
+     * @throws UnsupportedOperationException if the {@code set} operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of the specified element
+     *                                       prevents it from being added to this list
+     * @throws NullPointerException          if the specified element is null and
+     *                                       this list does not permit null elements
+     * @throws IllegalArgumentException      if some property of the specified
+     *                                       element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException     if the index is out of range
+     *                                       ({@code index < 0 || index >= size()})
+     */
     @Override
     public E set(int index, E element) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (element == null) {
+            throw new NullPointerException();
+        }
+        E oldElement = array[index];
+        array[index] = element;
+        return oldElement;
     }
 
+    /**
+     * Inserts the specified element at the specified position in this list
+     * (optional operation).  Shifts the element currently at that position
+     * (if any) and any subsequent elements to the right (adds one to their
+     * indices).
+     *
+     * @param index index at which the specified element is to be inserted
+     * @param element element to be inserted
+     * @throws UnsupportedOperationException if the {@code add} operation
+     *         is not supported by this list
+     * @throws ClassCastException if the class of the specified element
+     *         prevents it from being added to this list
+     * @throws NullPointerException if the specified element is null and
+     *         this list does not permit null elements
+     * @throws IllegalArgumentException if some property of the specified
+     *         element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *         ({@code index < 0 || index > size()})
+     */
     @Override
     public void add(int index, E element) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        if (element == null) {
+            throw new NullPointerException();
+        }
+        extendArrayIfNeeded();
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = element;
+        size++;
     }
 
     @Override
--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -3,6 +3,8 @@ package com.bdg;
 
 import CustomDataStructures.CustomArrayList;
 
+import java.util.ArrayList;
+
 /**
  * @author Ruzanna
  */
@@ -10,16 +12,44 @@ import CustomDataStructures.CustomArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        CustomArrayList<Integer> arr = new CustomArrayList<Integer>(2);
-        System.out.println(arr);
+        CustomArrayList<Integer> arr = new CustomArrayList<>(2);
+        System.out.println("*********************************************");
+        System.out.println("Elements are: " + arr);
+        System.out.println("Array size is: " + arr.size());
+        System.out.println("Number 15 contains in array: " + arr.contains(15));
+        System.out.println("*********************************************");
         arr.add(10);
         arr.add(15);
-        arr.add(5);
-        System.out.println(arr);
-        System.out.println(arr.contains(7));
-        System.out.println(arr);
-        arr.remove((Object) 15);
-        System.out.println(arr);
+        arr.add(1, 5);
+        arr.add(0, 9);
+        System.out.println("Elements are: " + arr);
+        System.out.println("Array size is: " + arr.size());
+        System.out.println("Number 15 contains in array: " + arr.contains(15));
+        System.out.println("*********************************************");
+        System.out.println("Element successfully removed: " + arr.remove((Object) 15));
+        System.out.println("Elements are: " + arr);
+        System.out.println("*********************************************");
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        arr2.add(1);
+        arr2.add(6);
+        arr2.add(18);
+        arr.addAll(arr2);
+        System.out.println("Elements are: " + arr);
+        System.out.println("Array size is: " + arr.size());
+        System.out.println("*********************************************");
+        arr.clear();
+        System.out.println("Elements are: " + arr);
+        System.out.println("Array size is: " + arr.size());
+        System.out.println("*********************************************");
+        arr.addAll(arr2);
+        System.out.println("Elements are: " + arr);
+        System.out.println("Array size is: " + arr.size());
+        System.out.println("*********************************************");
+        System.out.println("Element with index 1 is: " + arr.get(1));
+        System.out.println("*********************************************");
+        System.out.println("Elements are: " + arr);
+        System.out.println("Which element is changed: " + arr.set(0, 10));
+        System.out.println("Elements are: " + arr);
+        System.out.println("*********************************************");
     }
-
 }
