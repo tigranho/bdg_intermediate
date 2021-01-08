ba53de83b35fc2cd0e472bc3e6c8f49aa19dea84
arsenayan <arsen.avetisyan1@gmail.com>
2021-01-08 19:23:06 +0400

    my custom arrayList commit

--- src/main/java/com/bdg/homework/customCollections/MyArrayList/MyArrayList.java
+++ src/main/java/com/bdg/homework/customCollections/MyArrayList/MyArrayList.java
@@ -1,5 +1,12 @@
 package com.bdg.homework.customCollections.MyArrayList;
 
-public class MyArrayList
-{
+public class MyArrayList {
+    public static void main(String[] args) {
+        CustomArrayList customArrayList = new CustomArrayList();
+        System.out.println( customArrayList.isEmpty());
+        System.out.println( customArrayList.size());
+        System.out.println( customArrayList.contains(1));
+
+    }
+
 }
