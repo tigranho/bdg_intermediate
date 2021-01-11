77ec8ffd749b592a8c19d02ef41055e9a273c92c
arsenayan <arsen.avetisyan1@gmail.com>
2021-01-11 18:28:53 +0400

    LinkedList is finished

--- src/main/java/com/bdg/homework/customCollections/MyLinkedList/CustomLinkedList.java
+++ src/main/java/com/bdg/homework/customCollections/MyLinkedList/CustomLinkedList.java
@@ -2,158 +2,257 @@ package com.bdg.homework.customCollections.MyLinkedList;
 
 import java.util.*;
 
-public class CustomLinkedList <E> implements List<E> {
-    private Node headNode;
-    private Node tailNode;
-    private int size = 0;
-    private static final int DEFAULT_SIZE = 16;
-
-    public CustomLinkedList() {
-        headNode = null;
-        tailNode = null;
-    }
-
-     public void addFirst(E element){
-    	Node new_node= new  Node(null, null,element);
-    	new_node.next=headNode;
-    	new_node.prev=null;
-    	if (headNode!=null){
-    		headNode.prev=new_node;
-    		headNode=new_node;
-		}
-		 size++;
-   
-	 }
-	 
-	 @Override
-    public void add(int index, E element) {
-    Node new_node= new Node(null, null, element);
-    int current=0;
-
-    while (current<index){
-        headNode.next=headNode.next.next;
-        current++;
-
-        if (current<index-1){
-            headNode.next=new_node;
-            new_node.next=headNode.next.next;
-            headNode.next.next.prev=new_node;
-            new_node.prev=headNode;
-            size++;
-        }
-    }
- }
+public class CustomLinkedList<E> implements List<E>
+{
+	private Node headNode;
+	private Node tailNode;
+	private int size = 0;
+ 
+	
+	public CustomLinkedList()
+	{
+		headNode = null;
+		tailNode = null;
+	}
 	
 	@Override
-	public boolean add(E e) {
+	public boolean add(E e)
+	{
+		Node new_Node = new Node(e);
+		if (headNode == null)
+		{
+			headNode = new_Node;
+			
+		}
+		else
+		{
+			tailNode.next = new_Node;
+			new_Node.prev = tailNode;
+		}
+		tailNode = new_Node;
+		size++;
+		return true;
+		
+	}
+	
+	
+	public void addFirst(E element)
+	{
+		Node new_node = new Node(element);
+		new_node.next = headNode;
+		new_node.prev = null;
+		if (headNode != null)
+		{
+			headNode.prev = new_node;
+			headNode      = new_node;
+		}
+		size++;
+		
+	}
+	
+	@Override
+	public boolean remove(Object o)
+	{
+		Node removed_node = headNode;
+		while (size-- > 0)
+		{
+			if (removed_node.data == o)
+			{
+				removed_node.prev.next = removed_node.next;
+				removed_node.prev      = removed_node.prev.prev;
+				removed_node.next      = null;
+				removed_node.prev      = null;
+				size--;
+				return true;
+			}
+			removed_node = removed_node.next;
+			
+		}
+		
+		
 		return false;
 	}
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size==0;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
+	@Override
+	public boolean contains(Object o)
+	{
+		Node current = headNode;
+		
+		while (size-- > 0)
+		{
+			if (current.data == o)
+			{
+				return true;
+			}
+			current = current.next;
+		}
+		
+		
+		return false;
+	}
 	
 	@Override
-	public <T> T[] toArray(T[] a) {
+	public E get(int index)
+	{
+		Node result = headNode;
+		if (index == 0)
+		{
+			return (E) headNode.data;
+		}
+		while (index-- > 0)
+		{
+			result = result.next;
+			
+		}
+		return (E) result.data;
+		
+		
+	}
+	@Override
+	public void add(int index, E element)
+	{
+		Node new_node = new Node(element);
+		Node temNode = headNode;
+		int sum = 0;
+		
+		while (temNode.data == null)
+		{
+			
+			if (sum == index)
+			{
+				new_node.next = new_node;
+				
+				
+			}
+			sum++;
+			
+			temNode = tailNode.next;
+		}
+	}
+	
+	public void print()
+	{
+		Node current = headNode;
+		
+		while (current != null)
+		{
+			System.out.println(current.data.toString());
+			current = current.next;
+		}
+		
+	}
+	
+	@Override
+	public int size()
+	{
+		return size;
+	}
+	
+	@Override
+	public boolean isEmpty()
+	{
+		return size == 0;
+	}
+	
+
+	
+	@Override
+	public Iterator<E> iterator()
+	{
 		return null;
 	}
 	
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public E get(int index) {
-        return null;
-    }
-
-    @Override
-    public E set(int index, E element) {
-        return null;
-    }
-
-    @Override
-    public E remove(int index) {
-        return null;
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public ListIterator<E> listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator<E> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<E> subList(int fromIndex, int toIndex) {
-        return null;
-    }
+	@Override
+	public <T> T[] toArray(T[] a)
+	{
+		return null;
+	}
+	
+	
+	@Override
+	public Object[] toArray()
+	{
+		return new Object[0];
+	}
+	
+	
+	@Override
+	public boolean containsAll(Collection<?> c)
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean addAll(Collection<? extends E> c)
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c)
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean removeAll(Collection<?> c)
+	{
+		return false;
+	}
+	
+	@Override
+	public boolean retainAll(Collection<?> c)
+	{
+		return false;
+	}
+	
+	@Override
+	public void clear()
+	{
+	
+	}
+	
+	
+	@Override
+	public E set(int index, E element)
+	{
+		return null;
+	}
+	
+	@Override
+	public E remove(int index)
+	{
+		return null;
+	}
+	
+	@Override
+	public int indexOf(Object o)
+	{
+		return 0;
+	}
+	
+	@Override
+	public int lastIndexOf(Object o)
+	{
+		return 0;
+	}
+	
+	@Override
+	public ListIterator<E> listIterator()
+	{
+		return null;
+	}
+	
+	@Override
+	public ListIterator<E> listIterator(int index)
+	{
+		return null;
+	}
+	
+	@Override
+	public List<E> subList(int fromIndex, int toIndex)
+	{
+		return null;
+	}
 	
 	@Override
 	public boolean equals(Object o)
@@ -171,18 +270,19 @@ public class CustomLinkedList <E> implements List<E> {
 	}
 	
 	
-	
-	static class Node {
-        Node next;
-        Node prev;
-        Object data;
-
-        public Node(Node next, Node prev, Object data) {
-            this.next = next;
-            this.prev = prev;
-            this.data = data;
-        }
-
-    }
+	static class Node
+	{
+		Node next;
+		Node prev;
+		Object data;
+		
+		public Node(Object data)
+		{
+			this.next = null;
+			this.prev = null;
+			this.data = data;
+		}
+		
+	}
 }
 
--- src/main/java/com/bdg/homework/customCollections/MyLinkedList/MyLinkedList.java
+++ src/main/java/com/bdg/homework/customCollections/MyLinkedList/MyLinkedList.java
@@ -3,17 +3,19 @@ package com.bdg.homework.customCollections.MyLinkedList;
 import java.util.Iterator;
 import java.util.ListIterator;
 
-public class MyLinkedList {
-    public static void main(String[] args) {
-        CustomLinkedList<String> customLinkedList= new CustomLinkedList();
-
-       customLinkedList.addFirst("something");
-		System.out.println(customLinkedList);
-	
-		Iterator itr = customLinkedList.iterator();
-		while (itr.hasNext()){
-			System.out.println(itr.next());
-		}
-
-    }
+public class MyLinkedList
+{
+	public static void main(String[] args)
+	{
+		CustomLinkedList<String> customLinkedList = new CustomLinkedList();
+		
+		customLinkedList.add("1 Element");
+		customLinkedList.add("2 Element");
+		customLinkedList.add("3 Element");
+		customLinkedList.add("4 Element");
+		
+		customLinkedList.print();
+		
+		
+	}
 }
