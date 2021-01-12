87885d927723146ab7cb08c77b3d47958a97e8f5
tigranha <tik.hakobyan08@gmail.ru>
2021-01-12 15:38:15 +0400

    Modified CustomArrayList and added Javadoc

--- src/main/java/customCollections/CustomArrayList.java
+++ src/main/java/customCollections/CustomArrayList.java
@@ -2,28 +2,57 @@ package customCollections;
 
 import java.util.*;
 
+/**
+ * Resizable-array implementation of the List interface.
+ * Not synchronized.
+ * @param <E> the type of elements in this list which is instance of Object class
+ * @author Tigran
+ */
 public class CustomArrayList<E> implements List<E> {
 
     private E[] objects;
     private int size = 0;
+
     private static final int INITIAL_CAPACITY = 10;
 
 
-    CustomArrayList() {
+    public CustomArrayList() {
         objects = (E[]) new Object[INITIAL_CAPACITY];
     }
 
+    public CustomArrayList(int initialSize){
+        if (initialSize > 0){
+            this.objects = ((E[])new Object[initialSize]);
+        }else if (initialSize == 0){
+            this.objects = ((E[]) new Object[INITIAL_CAPACITY]);
+        }else {
+            throw new IllegalArgumentException();
+        }
+    }
 
+    /**
+     * Returns number of elements in the list.
+     * @return size.
+     */
     public int size() {
         return size;
     }
 
 
+    /**
+     * Checks if lise is empty or not.
+     * @return {@code true} if list has no elements.
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
 
+    /**
+     * Checks if list contains given param
+     * @param o element presence of which is to be checked
+     * @return {@code true} if list contains param.
+     */
     public boolean contains(Object o) {
         for (Object object : objects) {
             if (o.equals(object)) {
@@ -33,6 +62,11 @@ public class CustomArrayList<E> implements List<E> {
         return false;
     }
 
+    /**
+     * Appends given param to the end of the list.
+     * @param o element to be appended
+     * @return true (as specified by {@link Collection.add})
+     */
     public boolean add(Object o) {
         if (size == objects.length) {
             grow();
@@ -41,8 +75,17 @@ public class CustomArrayList<E> implements List<E> {
         return true;
     }
 
+    /**
+     * Appends all of the given collections' elements to the end of the list
+     * @param c collection that contains elements to be appended
+     * @return if the list has ben changed after the call
+     * @throws NullPointerException if the given collection is null
+     */
     public boolean addAll(Collection<? extends E> c) {
-        while (c.size() > objects.length) {
+        if (c == null ){
+            throw new NullPointerException();
+        }
+        if (c.size() > objects.length) {
             grow();
         }
         Object[] newObjects = c.toArray();
@@ -50,12 +93,24 @@ public class CustomArrayList<E> implements List<E> {
     }
 
 
+    /**
+     * Removes all the elements from the list and makes it empty.
+     */
     public void clear() {
-        objects = null;
+
+        for (int i = 0; i <size ; i++) {
+            objects[i] = null;
+            size = 0;
+        }
 
     }
 
-
+    /**
+     * Returns element in the given index.
+     * @param index the index of the element to be returned.
+     * @return element at the given param.
+     * @throws IndexOutOfBoundsException if {@code index>size || index<0}
+     */
     public E get(int index) {
         try {
             return (E) objects[index];
@@ -66,9 +121,16 @@ public class CustomArrayList<E> implements List<E> {
     }
 
 
+    /**
+     * Replaces the element in given position with specified element.
+     * @param index - index of the element to be replaced.
+     * @param element - element to be placed in given position.
+     * @return element that had been replaced.
+     * @throws IndexOutOfBoundsException if {@code (index>=objects.length || index<0)}
+     */
     public E set(int index, E element) {
-        if (index >= objects.length) {
-            throw new IllegalArgumentException();
+        if (index >= objects.length || index < 0) {
+            throw new IndexOutOfBoundsException();
         } else {
             objects[index] = element;
             return element;
@@ -86,14 +148,19 @@ public class CustomArrayList<E> implements List<E> {
     }
 
 
+    /**
+     * Inserts given element at specified position. Shifts the element
+     * currently at that position (if any) and any subsequent elements
+     * to the right (adds one to their indices).
+     * @param index - index where given element is be placed.
+     * @param element - element to be placed.
+     * @throws IndexOutOfBoundsException if {@code (index<0 || index>=size)}
+     */
     public void add(int index, E element) {
-        if (index < 0) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
-        while (index >= objects.length) {
-            grow();
-        }
-        Object temp;
+
         Object[] newObjects = new Object[objects.length];
         for (int i = 0; i < objects.length; i++) {
             if (index > i) {
@@ -108,7 +175,13 @@ public class CustomArrayList<E> implements List<E> {
 
     }
 
-
+    /**
+     * Removes element at given position and shifts next(if any)
+     * and all subsequent elements to the left (subtracts one from their indicies)
+     * @param index - position of the element to be removed
+     * @return - element that has been removed
+     * @throws IndexOutOfBoundsException if {@code (index >= objects.length)}
+     */
     public E remove(int index) {
 
         Object dummy;
