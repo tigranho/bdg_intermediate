ed69e8ca1808dd3185b596d6cdeae796385bb007
arammi <arammi@energizeglobal.com>
2021-01-21 20:17:23 +0400

    AramMirzoyan

--- src/main/java/com/bdg/threads/CurrentThreads.java
+++ src/main/java/com/bdg/threads/CurrentThreads.java
@@ -7,28 +7,27 @@ public class CurrentThreads {
         int number = 0;
         final Thr tr = new Thr();
         for (int i = 0; i < 6; i++) {
-            Runnable r = new Runnable() {
-                @Override
-                public void run() {
-                    tr.randomValue(new Random().nextInt(10)+1);
-                }
+            Runnable r = () -> {
+                tr.randomValue(new Random().nextInt(10)+1);
             };
             Thread thread = new Thread(r);
             thread.start();
             thread.join();
-            number+=tr.getValue();
-            System.out.println("the first number "+tr.getValue());
+            number += tr.getValue();
+            System.out.println("the first number " + tr.getValue());
         }
-        System.out.println("total value of number"+number);
+        System.out.println("total value of number" + number);
 
     }
 }
 
 class Thr {
     int value;
-     void randomValue(int value) {
+
+    void randomValue(int value) {
         this.value = value;
     }
+
     int getValue() {
         return value;
     }
