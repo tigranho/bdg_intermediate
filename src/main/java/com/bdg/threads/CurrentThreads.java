61aca0419d8ce1b61a4232f4c37182edade8af69
arammi <arammi@energizeglobal.com>
2021-01-21 19:25:43 +0400

    AramMirzoyan

--- src/main/java/com/bdg/threads/CurrentThreads.java
+++ src/main/java/com/bdg/threads/CurrentThreads.java
@@ -4,28 +4,37 @@ import java.util.Random;
 
 public class CurrentThreads {
     public static void main(String[] args) throws InterruptedException {
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
+        int number = 0;
+        final Thr tr = new Thr();
 
-            }
-        };
+        for (int i = 0; i < 6; i++) {
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    tr.randomValue(new Random().nextInt());
+                }
+            };
+            Thread thread = new Thread(r);
+            thread.start();
+            thread.join();
+        }
 
+        System.out.println(tr.getValue());
 
     }
 }
 
+class Thr {
 
-class RandomNumber {
+    int value;
 
-    private int randomNumber;
-
-    public synchronized void randomNumbers() {
-        this.randomNumber = new Random().nextInt();
+    synchronized void randomValue(int value) {
+        this.value = value;
     }
 
-    public synchronized int getRandomNumber() {
-        return this.randomNumber;
+    int getValue() {
+        return value;
     }
 
 }
+
