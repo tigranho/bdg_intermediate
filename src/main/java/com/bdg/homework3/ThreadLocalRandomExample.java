151fc717a000efb202717613e4841c1dd71765d4
Ara Chilingaryan <chilingaryanara96@gmail.com>
2021-01-21 10:08:33 +0400

    Threads

---
+++ src/main/java/com/bdg/homework3/ThreadLocalRandomExample.java
@@ -0,0 +1,30 @@
+package com.bdg.homework3;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadLocalRandomExample {
+
+    public static void main(String[] args) {
+        MyRunnable runnable = new MyRunnable();
+
+        Thread t[] = new Thread[5];
+        for (int i = 0; i < 5; i++) {
+                 t[i] = new Thread(runnable);
+                t[i].setName("MyRunnable-Thread-" + i);
+                t[i].start();
+            }
+        for (int i = 0; i < 5; i++) {
+            try {
+                t[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(runnable.sum);
+
+        }
+
+    }
+
+
