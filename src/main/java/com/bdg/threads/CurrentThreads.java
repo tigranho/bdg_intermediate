2ec88525d0cc54197b2fa7126fb255f84a9beae9
arammi <arammi@energizeglobal.com>
2021-01-21 19:29:43 +0400

    AramMirzoyan

--- src/main/java/com/bdg/threads/CurrentThreads.java
+++ src/main/java/com/bdg/threads/CurrentThreads.java
@@ -6,35 +6,31 @@ public class CurrentThreads {
     public static void main(String[] args) throws InterruptedException {
         int number = 0;
         final Thr tr = new Thr();
-
         for (int i = 0; i < 6; i++) {
             Runnable r = new Runnable() {
                 @Override
                 public void run() {
-                    tr.randomValue(new Random().nextInt());
+                    tr.randomValue(new Random().nextInt(10)+1);
                 }
             };
             Thread thread = new Thread(r);
             thread.start();
             thread.join();
+            number+=tr.getValue();
+            System.out.println("the first number "+tr.getValue());
         }
-
-        System.out.println(tr.getValue());
+        System.out.println("total value of number"+number);
 
     }
 }
 
 class Thr {
-
     int value;
-
     synchronized void randomValue(int value) {
         this.value = value;
     }
-
     int getValue() {
         return value;
     }
-
 }
 
