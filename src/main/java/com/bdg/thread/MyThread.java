046e63dffe5e3ec713621598d722b6fef5045e9d
Khoren Manukyan <xoren24@gmail.com>
2021-01-28 23:05:46 +0400

    Khoren - LinkedList methods implements

--- src/main/java/com/bdg/thread/MyThread.java
+++ src/main/java/com/bdg/thread/MyThread.java
@@ -3,20 +3,24 @@ package com.bdg.thread;
 import java.util.Random;
 
 public class MyThread {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
         int number = 0;
-        final ThreadTwo tr = new ThreadTwo();
-        for (int i = 0; i < 6; i++) {
+        ThreadTwo tread = new ThreadTwo();
+        for (int i = 0; i < 5; i++) {
             Runnable r = () -> {
-                tr.randomValue(new Random().nextInt(10)+1);
+                tread.randomValue(new Random().nextInt(11));
             };
             Thread thread = new Thread(r);
             thread.start();
-            thread.join();
-            number += tr.getValue();
-            System.out.println("the first number " + tr.getValue());
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            number += tread.getValue();
+            System.out.println("The first Thread number id: " + tread.getValue());
         }
-        System.out.println("total value of number" + number);
+        System.out.println("The value of sum is: " + number);
 
     }
 }
