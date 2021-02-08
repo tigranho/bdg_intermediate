f645888abfec791d58a2b88fabbf753db7b4f9dc
Khoren <xoren24@gmail.com>
2021-02-08 16:54:14 +0400

    Khoren - LinkedList methods implements

--- src/test/java/com/bdg/collections/and/map/collections/CustomArrayListTest.java
+++ src/test/java/com/bdg/collections/and/map/collections/CustomArrayListTest.java
@@ -1,11 +1,36 @@
 package com.bdg.collections.and.map.collections;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomArrayListTest {
+    public CustomArrayList<String> customArrayList() {
+        CustomArrayList<String> list = new CustomArrayList<>();
+        list.add("addElement1");
+        list.add("addElement2");
+        list.add("addElement3");
+        return list;
+    }
 
+    @Test
+    public void addElement() {
+        Assertions.assertTrue(customArrayList().add("addElement1"));
+        Assertions.assertTrue(customArrayList().add("addElement1"));
+    }
+
+    @Test
+    public  void removeElement(){
+        Assertions.assertTrue(customArrayList().remove("addElement1"));
+        Assertions.assertFalse(customArrayList().remove("removeElement"));
+    }
+
+    @Test
+    public  void  checkSize(){
+        Assertions.assertNotEquals(customArrayList().size(), 0);
+        Assertions.assertEquals(customArrayList().size(),3);
+    }
     @Test
     void add() {
     }
