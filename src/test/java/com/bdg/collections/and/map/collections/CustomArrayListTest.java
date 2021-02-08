7e19c37e63d90af6df96b275a3c3086b093da4a4
Khoren <xoren24@gmail.com>
2021-02-08 16:47:51 +0400

    Khoren - LinkedList methods implements

---
+++ src/test/java/com/bdg/collections/and/map/collections/CustomArrayListTest.java
@@ -0,0 +1,52 @@
+package com.bdg.collections.and.map.collections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomArrayListTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void indexOf() {
+    }
+}
