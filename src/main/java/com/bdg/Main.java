5b3457ec08ec67893510240dda52e3b9489856aa
arammi <arammi@energizeglobal.com>
2021-02-18 02:15:50 +0400

    AramMirzoyan

--- src/main/java/com/bdg/Main.java
+++ src/main/java/com/bdg/Main.java
@@ -1,8 +1,11 @@
 package com.bdg;
 
+import com.bdg.homework.hibernate_jpa.airport.excpetions.impl.AddressException;
 import com.bdg.homework.hibernate_jpa.airport.model.Address;
 import com.bdg.homework.hibernate_jpa.airport.repository.AddressDao;
 import com.bdg.homework.hibernate_jpa.airport.repository.impl.AddressDaoImpl;
+import com.bdg.homework.hibernate_jpa.airport.service.AddressService;
+import com.bdg.homework.hibernate_jpa.airport.service.impl.AddressServiceImpl;
 
 /**
  * @author Aram
@@ -12,9 +15,14 @@ public class Main {
     private String test;
 
     public static void main(String[] args) {
-        Address address = new Address("Yerevan", "Armenia");
-        AddressDao addressDao = new AddressDaoImpl();
-         addressDao.delete(4);
+        Address address = new Address("Yerevan", " ");
+        AddressService addressService= new AddressServiceImpl();
+        try {
+            addressService.save(address);
+        } catch (AddressException e) {
+           System.err.println(e);
+           System.out.println(e);
+        }
 
 
     }
